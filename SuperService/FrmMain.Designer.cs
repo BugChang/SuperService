2876b0f190fe6cd389547604123a633c73188640
mchangji <81069681@qq.com>
2017-09-06 16:06:16 +0800

    基本功能实现

---
+++ SuperService/FrmMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SuperService
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(371, 206);
+            this.txtLog.TabIndex = 0;
+            this.txtLog.Text = "";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "SuperService已启动";
+            this.notifyIcon1.BalloonTipTitle = "提示";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "SuperService";
+            this.notifyIcon1.Visible = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 206);
+            this.Controls.Add(this.txtLog);
+            this.Name = "FrmMain";
+            this.Text = "FrmMain";
+            this.Activated += new System.EventHandler(this.FrmMain_Activated);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
---
+++ SuperService/FrmMain.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using Fleck;
+using Newtonsoft.Json;
+
+namespace SuperService
+{
+    public partial class FrmMain : Form
+    {
+        #region 全局变量
+
+        private static SerialPort _serialPort;
+        private static IWebSocketConnection _iConnection;
+
+        #endregion
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 打开串口
+        /// </summary>
+        /// <param name="comName">串口名称</param>
+        /// <param name="baudRate">波特率</param>
+        public static void OpenSerialPort(string comName, int baudRate)
+        {
+            _serialPort?.Close();
+            _serialPort =
+                new SerialPort(comName, baudRate, Parity.None, 8, StopBits.One)
+                {
+                    Handshake = Handshake.RequestToSendXOnXOff
+                };
+            _serialPort.DataReceived += _serialPort_DataReceived;
+            _serialPort.Open();
+        }
+
+        /// <summary>
+        ///关闭串口
+        /// </summary>
+        public static void CloseSerialPort()
+        {
+            _serialPort.Close();
+        }
+
+        /// <summary>
+        /// 获取串口列表
+        /// </summary>
+        /// <returns></returns>
+        public static void GetSerialPortList()
+        {
+            string[] serialPortNames = SerialPort.GetPortNames();
+            var json = JsonConvert.SerializeObject(serialPortNames);
+            _iConnection.Send(json);
+        }
+
+        /// <summary>
+        /// 串口读取数据事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var text = _serialPort.ReadExisting();
+            var value = text.Replace("\u0002", "").Replace("\r\n\u0003", "");
+            _iConnection.Send(value);
+        }
+
+        /// <summary>
+        /// 启动WebSocket服务监听
+        /// </summary>
+        public static void StartWebSocket()
+        {
+            var server = new WebSocketServer("ws://0.0.0.0:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    _iConnection = socket;
+                };
+                socket.OnClose = () => { };
+                socket.OnMessage = HandleWebSocket;
+            });
+        }
+
+
+        public static void HandleWebSocket(string message)
+        {
+            var obj= JsonConvert.DeserializeObject<dynamic>(message);
+            switch (obj.command.ToString())
+            {
+                case "OpenSerialPort":
+                    var serialPortName = obj.serialPortName.ToString();
+                    var baudRate =int.Parse(obj.baudRate.ToString());
+                    OpenSerialPort(serialPortName, baudRate);
+                    break;
+                case "CloseSerialPort":
+                    CloseSerialPort();
+                    break;
+                case "GetSerialPortList":
+                    GetSerialPortList();
+                    break;
+            }
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+           
+            StartWebSocket();
+        }
+
+        private void FrmMain_Activated(object sender, EventArgs e)
+        {
+            Hide();
+        }
+    }
+}
